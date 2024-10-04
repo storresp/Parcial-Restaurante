e8b2dd503c8ef015b2b4e3fb1485d71d4426b4fd
storresp <164084726+storresp@users.noreply.github.com>
2024-10-04 08:51:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -159,7 +159,6 @@ document.addEventListener('DOMContentLoaded', function () {
             {
               method: 'GET',
               headers: {
-                'Content-Type': 'application/json',
                 'Access-Control-Allow-Origin': '*'
               }
             }
@@ -286,8 +285,7 @@ document.addEventListener('DOMContentLoaded', function () {
       fetch('https://script.googleusercontent.com/macros/echo?user_content_key=CKq-LQas5w4nUMnpY_-r0GUin-7rSoKPB7iBV_wXtrfbwojXs0-qOHR424skSoTiFA7eJEA2ER6ss48Xg6YhQibXYYv7Fu_4m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnA6wXRRzyrfTIKmLYEVRoswXqyofKrLrma4N-JjG6IUfb06gUu96E11OfZziasEjNI-G1zoKx9HpX2tur1IPZjIGRoGQdMN6zNz9Jw9Md8uu&lib=MqXoQJ202MbeZzBd6_irkBTKZ9dnBgvUF', { 
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Origin' : "*"
+          'Content-Type': 'application/json'
         },
         body: JSON.stringify(datosPago)
       })
