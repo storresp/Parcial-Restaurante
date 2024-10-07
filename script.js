13ffdff190a82a53e70cfdb60a1426e245229a68
storresp <164084726+storresp@users.noreply.github.com>
2024-10-07 12:20:49 -0500

    Update script.js

--- script.js
+++ script.js
@@ -157,8 +157,8 @@ document.addEventListener('DOMContentLoaded', function () {
       try {
         const response = await fetch('https://script.googleusercontent.com/macros/echo?user_content_key=CKq-LQas5w4nUMnpY_-r0GUin-7rSoKPB7iBV_wXtrfbwojXs0-qOHR424skSoTiFA7eJEA2ER6ss48Xg6YhQibXYYv7Fu_4m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnA6wXRRzyrfTIKmLYEVRoswXqyofKrLrma4N-JjG6IUfb06gUu96E11OfZziasEjNI-G1zoKx9HpX2tur1IPZjIGRoGQdMN6zNz9Jw9Md8uu&lib=MqXoQJ202MbeZzBd6_irkBTKZ9dnBgvUF',
             {
-              mode: 'no-cors',
               headers: {
+                'Access-Control-Allow-Origin': '*',
                 'Content-Type': 'application/json'
               }
             }
