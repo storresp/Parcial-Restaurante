80ac30735e6bf3a21911fac7228c54aca0b13d56
storresp <santiagotopo@unisabana.edu.co>
2024-10-07 18:57:59 -0500

    Update script.js

--- script.js
+++ script.js
@@ -157,10 +157,10 @@ document.addEventListener('DOMContentLoaded', function () {
       try {
         const response = await fetch('https://script.googleusercontent.com/macros/echo?user_content_key=CKq-LQas5w4nUMnpY_-r0GUin-7rSoKPB7iBV_wXtrfbwojXs0-qOHR424skSoTiFA7eJEA2ER6ss48Xg6YhQibXYYv7Fu_4m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnA6wXRRzyrfTIKmLYEVRoswXqyofKrLrma4N-JjG6IUfb06gUu96E11OfZziasEjNI-G1zoKx9HpX2tur1IPZjIGRoGQdMN6zNz9Jw9Md8uu&lib=MqXoQJ202MbeZzBd6_irkBTKZ9dnBgvUF',
             {
-              method: 'GET',
+              mode: 'no-cors',
               headers: {
                 'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*'
+                'Access-Control-Allow-Origin' : '*'
               }
             }
           );
@@ -209,13 +209,11 @@ document.addEventListener('DOMContentLoaded', function () {
           const nombre = item.querySelector('h3').textContent;
           const precioTexto = item.querySelector('p:nth-child(4)').textContent;
           
-          // Extraer el precio y eliminar comas
           let precio = '0';
           if (precioTexto.includes('$')) {
             precio = precioTexto.split('$')[1].replace(/,/g, ''); // Eliminar todas las comas
           }
           
-          // Convertir a número
           const precioNumero = parseFloat(precio);
           
           agregarAlCarrito(nombre, precioNumero);
@@ -282,36 +280,6 @@ document.addEventListener('DOMContentLoaded', function () {
         },
         pedido: cart
       };
-
-      fetch('https://script.googleusercontent.com/macros/echo?user_content_key=CKq-LQas5w4nUMnpY_-r0GUin-7rSoKPB7iBV_wXtrfbwojXs0-qOHR424skSoTiFA7eJEA2ER6ss48Xg6YhQibXYYv7Fu_4m5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnA6wXRRzyrfTIKmLYEVRoswXqyofKrLrma4N-JjG6IUfb06gUu96E11OfZziasEjNI-G1zoKx9HpX2tur1IPZjIGRoGQdMN6zNz9Jw9Md8uu&lib=MqXoQJ202MbeZzBd6_irkBTKZ9dnBgvUF', { 
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Access-Control-Origin' : "*"
-        },
-        body: JSON.stringify(datosPago)
-      })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`Error en el servidor: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then(data => {
-        if (data.status === 'success') {
-          alert('Pago confirmado. ¡Gracias por tu compra!');
-          vaciarCarrito();
-          window.location.href = 'index.html'; 
-        } else {
-          throw new Error('Error al procesar el pedido.');
-        }
-      })
-      .catch(error => {
-        console.error('Error al procesar el pago:', error);
-        alert('Hubo un error al procesar tu pago. Por favor, inténtalo de nuevo.');
-      });
     });
   }
 });
-
-
